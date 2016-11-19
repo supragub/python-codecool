32621c68f7d0ea5f8630a149f8f2adf564677fcb
Gábor Varga <gabor.varga@suprasoft.hu>
2016-11-19 23:24:49 +0100

    Testing IsOdd method

---
+++ 14-Preps4teamwork/test.py
@@ -0,0 +1,25 @@
+import unittest
+
+# Here's our "unit".
+
+
+def IsOdd(n):
+    return
+
+# Here's our "unit tests".
+
+
+class IsOddTests(unittest.TestCase):
+
+    def testOne(self):
+        self.failUnless(IsOdd(1))
+
+    def testTwo(self):
+        self.failIf(IsOdd(2))
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
