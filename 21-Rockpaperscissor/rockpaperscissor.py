2ab88f57fc9dd76f4cab8b604efd1cfb14f0ed3f
Gábor Varga <gabor.varga@suprasoft.hu>
2016-11-22 23:36:07 +0100

    Rock Paper Scissor

--- 21-Rockpaperscissor/rockpaperscissor.py
+++ 21-Rockpaperscissor/rockpaperscissor.py
@@ -1,31 +1,38 @@
-    player1 = input("Player 1: ?")
-    player2 = input("Player 2: ?")
+def rockpaperscissor():
+
+    print("Choice: rock, paper, scissors")
+
+    player1 = input("Player 1: ")
+    player2 = input("Player 2: ")
 
     if (player1 == 'rock' and player2 == 'scissors'):
-        print "Player 1 wins."
+        print("Player 1 wins.")
 
     elif (player1 == 'rock' and player2 == 'rock'):
-        print "Tie"
+        print("Tie")
 
     elif (player1 == 'scissors' and player2 == 'paper'):
-        print "Player 1 wins."
+        print("Player 1 wins.")
 
     elif (player2 == 'scissors' and player2 == 'scissors'):
-        print "Tie"
+        print("Tie")
 
     elif (player1 == 'paper' and player2 == 'paper'):
-        print "Tie"
+        print("Tie")
 
     elif (player1 == 'paper' and player2 == 'scissors'):
-        print "Player 2 wins."
+        print("Player 2 wins.")
 
     elif (player1 == 'rock'and player2 == 'paper'):
-        print "Player 2 wins."
+        print("Player 2 wins.")
 
     elif (player1 == 'paper' and player2 == 'rock'):
-        print "Player 2 wins."
+        print("Player 2 wins.")
 
     elif (player1 == 'scissors' and player2 == 'rock'):
-        print "Player 2 wins."
+        print("Player 2 wins.")
     else:
-        print "This is not a valid object selection."
+        print("This is not a valid object selection.")
+
+
+rockpaperscissor()
